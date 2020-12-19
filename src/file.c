b4be99d7150f3819b20b4f26dba5b12884e293b2
Kalle Rustholkarhu <krusthol@student.hive.fi>
2020-12-20 00:22:55 +0200

    Redeploying with debugging to help identify asset compression or similar issue preventing data pristinity at production server

--- src/file.c
+++ src/file.c
@@ -21,6 +21,8 @@ static int	correct_char(char c)
 {
 	static int amount_of_p;
 
+	if (c != 'W' && c != 'P' && c != '.')
+		printf("Incorrect char culprit: %c\n", c);
 	if (!c)
 		return (amount_of_p);
 	if (c == 'P')
@@ -45,6 +47,22 @@ static int	load_map_line(char *buff, int (correct)(char c))
 	return (0);
 }
 
+static void print_data(char *data, int cols, int rows)
+{
+	int i = 0;
+	printf("%d x %d COLS, ROWS\n", cols, rows);
+	puts("Received data");
+	while (*data)
+	{
+		if (i != 0 && i % cols == 0)
+			write(1, "\n", 1);
+		write(1, data, 1);
+		data++;
+		i++;
+	}
+	puts("\nEnd of data");
+}
+
 static int	read_map(int opened, t_view *view)
 {
 	char	*buff;
@@ -56,12 +74,13 @@ static int	read_map(int opened, t_view *view)
 	line = 0;
 	while (get_next_line(opened, &buff))
 	{
-		if (!load_map_line(buff, line++ == 0 ? correct_edge : correct_char))
-			close_and_exit(opened, MAP_CONTENT);
+		//if (!load_map_line(buff, line++ == 0 ? correct_edge : correct_char))
+		load_map_line(buff, line++ == 0 ? correct_edge : correct_char);
+		//	close_and_exit(opened, MAP_CONTENT);
 		if ((int)ft_strlen(buff) == view->g->cols || view->g->cols == 0)
 			view->g->cols = ft_strlen(buff);
-		else
-			close_and_exit(opened, MAP_SHAPE);
+		//else
+		//	close_and_exit(opened, MAP_SHAPE);
 		view->g->rows++;
 		temp = data;
 		data = ft_strjoin(data, buff);
@@ -69,6 +88,7 @@ static int	read_map(int opened, t_view *view)
 		free(buff);
 	}
 	view->g->grid = grid_set(view->g->rows, view->g->cols, data);
+	print_data(data, view->g->cols, view->g->rows);
 	free(data);
 	close(opened);
 	return (0);
@@ -87,9 +107,11 @@ int			check_mapfile(char *file_arg, t_view *view)
 		is_bad = read_map(opened, view);
 		if (!is_bad)
 		{
-			if (!load_map_line(view->g->grid[view->g->rows - 1], correct_edge)
-				|| correct_char(0) != 1)
-				return (MAP_CONTENT);
+			//if (!load_map_line(view->g->grid[view->g->rows - 1], correct_edge))
+			//	puts("Bottom edge culprit!");
+			//if (correct_char(0) != 1)
+			//	puts("Culprit incorrect P amount!");
+			//return (MAP_CONTENT);
 			if (view->g->rows < 3 || view->g->cols < 3 ||
 				view->g->rows > 256 || view->g->cols > 256)
 				return (MAP_SIZE);
